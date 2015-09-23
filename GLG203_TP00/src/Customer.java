78b1210b9230b78964a4075d089a4fc96b97b16a
Fabo <Fabo@fbd718d7-1730-4f50-90c8-b28562076828>
2015-09-23 07:23:39 +0000

    Début de TP00

---
+++ GLG203_TP00/src/Customer.java
@@ -0,0 +1,82 @@
+
+public class Customer {
+
+    private String _id;
+    public String get_id() {
+		return _id;
+	}
+	public void set_id(String _id) {
+		this._id = _id;
+	}
+	public String get_firstname() {
+		return _firstname;
+	}
+	public void set_firstname(String _firstname) {
+		this._firstname = _firstname;
+	}
+	public String get_lastname() {
+		return _lastname;
+	}
+	public void set_lastname(String _lastname) {
+		this._lastname = _lastname;
+	}
+	public String get_telephone() {
+		return _telephone;
+	}
+	public void set_telephone(String _telephone) {
+		this._telephone = _telephone;
+	}
+	public String get_street1() {
+		return _street1;
+	}
+	public void set_street1(String _street1) {
+		this._street1 = _street1;
+	}
+	public String get_street2() {
+		return _street2;
+	}
+	public void set_street2(String _street2) {
+		this._street2 = _street2;
+	}
+	public String get_city() {
+		return _city;
+	}
+	public void set_city(String _city) {
+		this._city = _city;
+	}
+	public String get_state() {
+		return _state;
+	}
+	public void set_state(String _state) {
+		this._state = _state;
+	}
+	public String get_zipcode() {
+		return _zipcode;
+	}
+	public void set_zipcode(String _zipcode) {
+		this._zipcode = _zipcode;
+	}
+	public String get_country() {
+		return _country;
+	}
+	public void set_country(String _country) {
+		this._country = _country;
+	}
+	public String get_mail() {
+		return _mail;
+	}
+	public void set_mail(String _mail) {
+		this._mail = _mail;
+	}
+	private String _firstname;
+    private String _lastname;
+    private String _telephone;
+    private String _street1;
+    private String _street2;
+    private String _city;
+    private String _state;
+    private String _zipcode;
+    private String _country;
+    private String _mail;
+	
+}
