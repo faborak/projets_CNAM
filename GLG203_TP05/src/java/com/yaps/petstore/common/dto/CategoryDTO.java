c8bd98021e9127c09417b6dd9ef4381115d4ca3c
fabo <Fabo@fbd718d7-1730-4f50-90c8-b28562076828>
2015-11-15 13:03:43 +0000

    Fin des devs

--- GLG203_TP05/src/java/com/yaps/petstore/common/dto/CategoryDTO.java
+++ GLG203_TP05/src/java/com/yaps/petstore/common/dto/CategoryDTO.java
@@ -1,34 +1,57 @@
 package com.yaps.petstore.common.dto;
 
-public class CategoryDTO implements DataTransfertObject{
+/**
+ * This class follows the Data Transfert Object design pattern and for that implements the
+ * markup interface DataTransfertObject. It is a client view of an Category. This class only
+ * transfers data from a distant service to a client.
+ */
+public class CategoryDTO implements DataTransfertObject {
 
-	public CategoryDTO(String string, String string2, String string3) {
+	// ======================================
+	// = Attributes =
+	// ======================================
+	private String _id;
+	private String _name;
+	private String _description;
+
+    // ======================================
+    // =            Constructors            =
+    // ======================================
+    public CategoryDTO() {
 		// TODO Auto-generated constructor stub
 	}
-
-	public void setName(String string) {
-		// TODO Auto-generated method stub
-		
+	
+	public CategoryDTO(String id, String name, String description) {
+		this._id = id;
+		this._name = name;
+		this._description = description;
 	}
 
-	public void setDescription(String string) {
-		// TODO Auto-generated method stub
-		
+	// ======================================
+    // =         Getters and Setters        =
+    // ======================================
+	public void setName(String name) {
+		this._name = name;
+	}
+
+	public void setDescription(String description) {
+		this._description = description;
 	}
 
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return _name;
 	}
 
 	public String getDescription() {
-		// TODO Auto-generated method stub
-		return null;
+		return _description;
+	}
+
+	public void setId(String id) {
+		this._id = id;
 	}
 
 	public String getId() {
-		// TODO Auto-generated method stub
-		return null;
+		return _id;
 	}
 
 }
