c6ea688ea57e64e570f5390f64f2ff3c851685f2
Fabo <Fabo@fbd718d7-1730-4f50-90c8-b28562076828>
2015-12-05 14:51:52 +0000

    Retour arrière : Barbank et Yaps dans le projet

---
+++ GLG203_TP08_versionJava/Yaps/resources/item.jsp
@@ -0,0 +1,45 @@
+<%@ page isProductPage="true" %>
+<%@page import="java.util.Collection"%>
+<%@page import="java.util.List"%>
+<%@ page import="com.yaps.petstore.common.dto.ItemDTO"%>
+<%@ page import="com.yaps.petstore.common.dto.ProductDTO"%>
+<html>
+<head>
+	<title>YAPS Error</title>
+</head>
+<body>
+
+<table cellspacing="0" cellpadding="5" width="100%">
+    <%--HEADER--%>
+	<tr>
+		<td colspan="3">
+			<jsp:include page="common/header.jsp"/>
+		</td>
+	</tr>
+
+	<tr>
+        <%--NAVIGATION--%>
+        <td valign="top" width="20%">
+    		<jsp:include page="common/navigation.jsp"/>
+    	</td>
+
+        <td align="left" valign="top" width="60%">
+        <%--CENTRAL BODY--%>
+
+              <%  ItemDTO itemDTO =  (ItemDTO) request.getAttribute("Item");%> 
+                     <% out.println(itemDTO.getName()); %> 
+                     <img src=" <%itemDTO.getImagePath();%> " /> 
+                     Unit Cost :  <%   out.println(itemDTO.getUnitCost());  %> 
+
+    <%--FOOTER--%>
+    	</td>
+        <td></td>
+    </tr>
+    <tr>
+    	<td colspan="3">
+    		<jsp:include page="common/footer.jsp"/>
+    	</td>
+    </tr>
+</table>
+</body>
+</html>
---
+++ GLG203_TP08_versionJava/Yaps/src/item/Item.java
@@ -0,0 +1,100 @@
+package com.yaps.petstore.server.domain.item;
+
+import java.io.Serializable;
+
+import com.yaps.petstore.server.domain.DomainObject;
+import com.yaps.petstore.server.domain.product.Product;
+import com.yaps.petstore.common.exception.CheckException;
+
+/**
+ * This class represents an Item in the catalog of the YAPS company.
+ * The catalog is divided into catagories. Each one divided into products
+ * and each product in items.
+ */
+public final class Item extends DomainObject implements Serializable {
+
+    // ======================================
+    // =             Attributes             =
+    // ======================================
+    private String _name;
+    private double _unitCost;
+    private String _imagePath;
+    private Product _product;
+
+    // ======================================
+    // =            Constructors            =
+    // ======================================
+    public Item() {
+    }
+
+    public Item(final String id) {
+        setId(id);
+    }
+
+    public Item(final String id, final String name, final double unitCost, final Product product) {
+        setId(id);
+        setName(name);
+        setUnitCost(unitCost);
+        setProduct(product);
+    }
+
+    // ======================================
+    // =           Business methods         =
+    // ======================================
+    public void checkData() throws CheckException {
+        if (getName() == null || "".equals(getName()))
+            throw new CheckException("Invalid name");
+        if (getUnitCost() <= 0)
+            throw new CheckException("Invalid unit cost");
+        if (getProduct() == null || getProduct().getId() == null || "".equals(getProduct().getId()))
+            throw new CheckException("Invalid product");
+    }
+
+    // ======================================
+    // =         Getters and Setters        =
+    // ======================================
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(final String name) {
+        _name = name;
+    }
+
+    public double getUnitCost() {
+        return _unitCost;
+    }
+
+    public void setUnitCost(final double unitCost) {
+        _unitCost = unitCost;
+    }
+
+    public String getImagePath() {
+        return _imagePath;
+    }
+
+    public void setImagePath(final String imagePath) {
+        _imagePath = imagePath;
+    }
+
+    public Product getProduct() {
+        return _product;
+    }
+
+    public void setProduct(final Product product) {
+        _product = product;
+    }
+
+    public String toString() {
+        final StringBuffer buf = new StringBuffer();
+        buf.append("\n\tItem {");
+        buf.append("\n\t\tId=").append(getId());
+        buf.append("\n\t\tName=").append(getName());
+        buf.append("\n\t\tUnit Cost=").append(getUnitCost());
+        buf.append("\n\t\timagePath=").append(getImagePath());
+        buf.append("\n\t\tProduct Id=").append(getProduct().getId());
+        buf.append("\n\t\tProduct Name=").append(getProduct().getName());
+        buf.append("\n\t}");
+        return buf.toString();
+    }
+}
